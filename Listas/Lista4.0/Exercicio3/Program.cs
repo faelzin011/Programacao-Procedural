151bac786a06903511ac41a8354828472970eb1e
faelzin011 <rafaelvasconceloscoutinho@gmail.com>
2024-03-29 21:21:01 -0300

    Lista 4- laços de repetiçao alinhados

---
+++ Listas/Lista4.0/Exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    for(int linha = 0; linha <8; linha++)
+    {
+      for(int coluna =0; coluna <8; coluna++)
+      if(coluna == 0 || coluna == 7 || linha == 7 || linha == 0)
+      {
+        Tabuleiro.Adicionar(linha, coluna, "C");
+      }
+    }
+
+    Tabuleiro.ImprimeTabuleiro();
+  }  
+}
+
+    
