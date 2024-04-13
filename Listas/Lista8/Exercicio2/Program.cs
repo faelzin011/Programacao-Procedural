7aec00cd1417ae9f795702a5386339f06cf326f6
faelzin011 <rafaelvasconceloscoutinho@gmail.com>
2024-04-12 23:23:55 -0300

    Lista 8- Structss

---
+++ Listas/Lista8/Exercicio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto[] produtos = new Produto[4];
+
+        for(int i =0; i < produtos.Length; i++)
+        {
+            produtos[i].id = Convert.ToInt32(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+            produtos[i].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+        }
+
+        for(int i =0; i < produtos.Length; i++)
+        {
+            if(produtos[i].disponivelEmEstoque == true)
+            {   
+                System.Console.WriteLine("\n");
+                System.Console.WriteLine("Produtos que estão disponiveis: ");
+                System.Console.WriteLine("\n");
+                System.Console.WriteLine(produtos[i].preco);
+                System.Console.WriteLine(produtos[i].nome);
+            }
+        }
+        
+        
+    }
+}
